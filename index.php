821ac35196948d2039a65f4d0ba2833e5dd0d32c
Eagle19243 <eagle19243@gmail.com>
2018-04-04 12:02:28 -0700

    add resource, page creation

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Membership Sales</title>
+    </head>
+    <body>
+        <a href="./MembershipSales.html">
+            Membership Sales
+        </a><br><br>
+    </body>
+</html>
